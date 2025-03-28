b86fa5f494414a3054ea44d5a02ff36fa7ae7898
Hua Thinh Hung <huahung0601@gmail.com>
2025-03-28 08:16:43 +0700

    Initial commit

---
+++ YTeAspMVC/Migrations/202406201939567_DBYTeAspMVC.cs
@@ -0,0 +1,18 @@
+﻿namespace YTeAspMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DBYTeAspMVC : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Bookings", "BookingTime", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Bookings", "BookingTime");
+        }
+    }
+}
